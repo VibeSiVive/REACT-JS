fc1ab932671c519508f14849770cf9fa9a7827e8
VibeSiVive <vbbataga2@JF610B-030>
2024-09-05 18:10:49 +0800

    first commit

---
+++ first-react-project/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Car from "./components/Car";
+import CarClass from "./components/CarClass"
+
+function App() {
+  return <CarClass />;
+}
+
+export default App;
---
+++ first-react-project/src/components/CarClass.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+class CarClass extends React.Component {
+    render() {
+        return <h2>Hi, I'm a class car</h2>
+    }
+}
+export default CarClass;
